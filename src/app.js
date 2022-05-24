90846ad5d267287370170d8d0b2c89f33477222d
Maruf Al Hossain <pmaruf42@gmail.com>
2022-05-24 11:03:27 +0600

    styles added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+
+const API_URL = 'http://www.omdbapi.com/?apikey=5656a896';
 
 const App = () => {
+
+  const searchMovie = async (title) => {
+    const url = `${API_URL}&s=${title}`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    console.log(data.Search);
+  }
+
+  useEffect(() => {
+      searchMovie('spider-man');
+  }, []);
+
   return (
     <div>
       <h1>App</h1>
